9e0df90d0e297d44e00a9164ea6318ef74dfb858
Giver Cifuentes <gdcifuentes7@misena.edu.co>
2020-06-17 10:11:38 -0500

    clase 17-06-2020

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Product;
+use Faker\Generator as Faker;
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence(5),
+        'description' => $faker->sentence(50),
+        'price' => $faker->randomFloat($maxDecimals =2, $min=4, $max=100),
+        'sctok' => $faker->numberBetween(1,10),
+        'status' => $faker->randomElement(['disponible','no disponible']),
+    ];
+});
